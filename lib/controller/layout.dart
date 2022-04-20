53fc5be770aa581cdf3afb0e43cae1dd3cc17404
バンクー　ゴルタン <ban.kuu@yahoo.com>
2022-04-20 12:08:07 +0700

    set no startup phase is ok

--- lib/controller/layout.dart
+++ lib/controller/layout.dart
@@ -12,27 +12,80 @@ class HomeScreenControllerBinding implements Bindings {
 
 class HomeScreenController extends GetxController {
   late Timer? _timer;
-  var textOpacity = 0.00.obs;
-  late RiveAnimationController welcomeFooter;
+  var textOpacity = 1.00.obs;
+  late _SpeedController welcomeFooter;
+  var isEntering = false.obs;
+  var isToMainMenu = false.obs;
 
   HomeScreenController._internal();
 
   @override
   void onInit() {
     super.onInit();
-    _timer = Timer.periodic(const Duration(milliseconds: 750), (timer) {
-      if (textOpacity.value == 0) {
-        textOpacity.value = 1;
+    welcomeFooter = _SpeedController('runner', speedMultiplier: 1);
+  }
+
+  onEnter() {
+    if (isEntering.isTrue) {
+      return;
+    }
+
+    isEntering.value = true;
+
+    // Flash Text
+
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      if (timer.tick % 3 == 0) {
+        if (textOpacity.value == 0) {
+          textOpacity.value = 1;
+        } else {
+          textOpacity.value = 0;
+        }
+      }
+
+      if (welcomeFooter.speedMultiplier.value > 0.1 && isToMainMenu.isFalse) {
+        welcomeFooter.speedMultiplier.value = welcomeFooter.speedMultiplier.value - (welcomeFooter.speedMultiplier.value / 8);
       } else {
-        textOpacity.value = 0;
+        if (isToMainMenu.isFalse) {
+          welcomeFooter.speedMultiplier.value = 0.01;
+          isToMainMenu.value = true;
+        }
+
+        if (welcomeFooter.speedMultiplier.value < 8) {
+          welcomeFooter.speedMultiplier.value = welcomeFooter.speedMultiplier.value + 0.25;
+        }
       }
     });
-    welcomeFooter = OneShotAnimation('runner', autoplay: true);
+
+    // Create enter Animation
+    // welcomeFooter.changeSpeed(5);
   }
 
   @override
   void dispose() {
     _timer?.cancel();
     super.dispose();
+  }
+}
+
+class _SpeedController extends SimpleAnimation {
+  final speedMultiplier = Rx<double>(1);
+
+  _SpeedController(
+    String animationName, {
+    double mix = 1,
+    double speedMultiplier = 1,
+  }) : super(animationName, mix: mix) {
+    this.speedMultiplier.value = speedMultiplier;
+  }
+
+  @override
+  void apply(RuntimeArtboard artboard, double elapsedSeconds) {
+    if (instance == null || !instance!.keepGoing) {
+      isActive = false;
+    }
+    instance!
+      ..animation.apply(instance!.time, coreContext: artboard, mix: mix)
+      ..advance(elapsedSeconds * speedMultiplier.value);
   }
 }
--- lib/view/page/welcome.dart
+++ lib/view/page/welcome.dart
@@ -6,74 +6,96 @@ import 'package:get/get.dart';
 import 'package:rive/rive.dart' as rive;
 
 class WelcomePage extends StatelessWidget {
+  final FocusNode _focusNode = FocusNode();
+
   final HomeScreenController controller;
 
-  const WelcomePage(this.controller, {Key? key}) : super(key: key);
+  WelcomePage(this.controller, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 80),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              ShaderMask(
-                blendMode: BlendMode.srcIn,
-                shaderCallback: (bounds) => LinearGradient(colors: [
-                  ColorSet.textBegin.color,
-                  ColorSet.textEnd.color,
-                ]).createShader(
-                  Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        "200 in 1 : Curriculum Vitae",
-                        style: TextStyle(fontSize: 40, height: 0.75),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text("BANKUU", style: TextStyle(fontSize: 350, height: 0.6)),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return RawKeyboardListener(
+      autofocus: true,
+      onKey: (value) {
+        controller.onEnter();
+      },
+      focusNode: _focusNode,
+      child: GestureDetector(
+        onTap: () {
+          controller.onEnter();
+        },
+        behavior: HitTestBehavior.opaque,
+        child: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 80),
+              child: Stack(
+                alignment: Alignment.center,
                 children: [
-                  SizedBox(height: MediaQuery.of(context).size.height / 2.25),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Obx(
-                      () => Opacity(
-                        opacity: controller.textOpacity.value,
-                        child: Text(
-                          "Any key or click to enter... ",
-                          style: TextStyle(fontSize: 30, color: ColorSet.textEnd.color),
+                  ShaderMask(
+                    blendMode: BlendMode.srcIn,
+                    shaderCallback: (bounds) => LinearGradient(colors: [
+                      ColorSet.textBegin.color,
+                      ColorSet.textEnd.color,
+                    ]).createShader(
+                      Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                    ),
+                    child: TweenAnimationBuilder<double>(
+                      tween: Tween<double>(begin: 0, end: 1),
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.easeOutCubic,
+                      builder: (BuildContext context, double size, Widget? child) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text(
+                                "Curriculum Vitae",
+                                style: TextStyle(fontSize: size * 40, height: 0.75),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text("BANKUU", style: TextStyle(fontSize: size * 450, height: 0.6)),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.height / 2.25),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Obx(
+                          () => Opacity(
+                            opacity: controller.textOpacity.value,
+                            child: Text(
+                              controller.isEntering.isTrue ? "Please wait..." :"Any key or click to enter",
+                              style: TextStyle(fontSize: 30, color: ColorSet.textEnd.color),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
-        rive.RiveAnimation.asset(
-          'asset/rive/welcome-footer.riv',
-          fit: BoxFit.fill,
-          controllers: [
-            controller.welcomeFooter,
+            ),
+            rive.RiveAnimation.asset(
+              'asset/rive/welcome-footer.riv',
+              fit: BoxFit.fill,
+              controllers: [
+                controller.welcomeFooter,
+              ],
+            )
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }
